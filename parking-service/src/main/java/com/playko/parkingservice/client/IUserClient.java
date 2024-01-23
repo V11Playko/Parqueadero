96996838e5f78c9d43201278bfdc48dcc08d0052
Heinner Vega <heinnervega20@gmail.com>
2024-01-23 11:34:58 -0500

    HU06 Added a new validation and fixed an endpoint url

--- parking-service/src/main/java/com/playko/parkingservice/client/IUserClient.java
+++ parking-service/src/main/java/com/playko/parkingservice/client/IUserClient.java
@@ -15,6 +15,6 @@ public interface IUserClient {
 
     @GetMapping("/admin/user-by-email")
     Optional<User> getUserByEmail(@RequestParam String email);
-    @GetMapping("/partner/getUser")
+    @GetMapping("/partner/get-user")
     Optional<User> getUser(@RequestParam String email);
 }
